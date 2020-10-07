751a8c2f583e71dc180cef67c6538f91c094deb0
AngelFernandezGBTEC <angel.fernandez@gbtec.es>
2020-10-07 12:41:49 +0200

    chore: development version update

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-docker build . --tag xovetic-lba-apigateway:1.1.0
-docker tag xovetic-lba-apigateway:1.1.0 angelfernandezf/xovetic-lba-apigateway:1.1.0
-docker push angelfernandezf/xovetic-lba-apigateway:1.1.0
+docker build . --tag xovetic-lba-apigateway:1.2.0
+docker tag xovetic-lba-apigateway:1.2.0 angelfernandezf/xovetic-lba-apigateway:1.2.0
+docker push angelfernandezf/xovetic-lba-apigateway:1.2.0
